27ffcc4eaf54cba05fe206994211ac073cedd792
colemitch <colemitch03@outlook.com>
2020-10-08 09:28:03 +0100

    Created first graphics window

--- Project.py
+++ Project.py
@@ -1,1 +1,17 @@
+#importing pygame module
 import pygame
+from pygame.locals import *
+
+pygame.init()
+running = True
+window = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("window")
+while running == True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            running = False
+        
+        
+
